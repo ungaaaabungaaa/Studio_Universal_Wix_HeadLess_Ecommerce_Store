bff4d47bb9df31a2dc9b00ab89933230e5b772aa
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-11-10 23:46:25 +0530

    disabled ts in jsx components cart model

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -104,7 +104,7 @@ const CartModal = () => {
           <div className="">
             <div className="flex items-center justify-between font-semibold">
               <span className="">Subtotal</span>
-              // @ts-ignore
+              {/* @ts-ignore */}
               <span className="">${cart.subtotal?.amount || 0}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
